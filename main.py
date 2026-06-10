58da75e8469843a4aeb70415e5bdef0aa6b5d093
Edison Furusato <edison.oda@hotmail.com>
2026-06-10 00:10:00 -0300

    Actual base for the project

--- main.py
+++ main.py
@@ -1,54 +1,46 @@
 import os
+import csv
 
-# importa classes
-from vs.environment import Env
-from exp import Explorer
-from soc import Rescuer
+def main(map_file):
+    victims_list = []
 
+    with open(map_file, 'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+        next(csvreader)  # Pula a primeira linha (cabeçalho)
 
-def main(vict_folder, env_folder, config_ag_folder):
-    # Instantiate the environment
-    env = Env(vict_folder, env_folder)
+        for row in csvreader:
+            x = int(row[0])  # coordenada x
+            y = int(row[1])  # coordenada y
+            obst = float(row[2])  # obstaculo
+            id = int(row[3])   # victim id number
+            idade = int(row[4])
+            fc = int(row[5])    # freq cardiaca
+            fr = int(row[6])    # freq respiratoria
+            pas = int(row[7])   # pressao arterial sistolica
+            spo2 = int(row[8])  # saturacao de oxigenio
+            temp = float(row[9])  # temperatura corporal
+            pr = int(row[10])    # pulso radial (0 ou 1)
+            sg = int(row[11])    # sangramento (0 ou 1)
+            fx = int(row[12])    # fratura exposta (0 ou 1)
+            queim = int(row[13])  # queimardura (niveis)
+            gcs = int(row[14])   # Coma - Glasgow scale
+            avpu = int(row[15])  # estado de consciencia
+            tri = int(row[16])  # Triagem START: 0 GRN, 1 YEL, 2 RED, 3 BLK
+            sobr = float(row[17])  # Prob. de sobrevivencia
 
-    # config files for the agents
-    cfg_exp = []   # arquivos de cfg dos exploradores
-    cfg_soc = []   # arquivos de cfg dos socorristas
-    exp = []       # agentes exploradores
-    soc = []       # agentes socorristas
-    
-    for i in range(3):
-        cfg_exp.append(os.path.join(config_ag_folder, f"exp_{i+1}.txt"))
-        cfg_soc.append(os.path.join(config_ag_folder, f"soc_{i+1}.txt"))
-        
-        # instancia um socorrista e salva na lista
-        soc.append(Rescuer(env, cfg_soc[i]))
-        
-        # exploradores precisam conhecer o socorrista mestre (0) 
-        # para enviarem o mapa de exploração
-        exp.append(Explorer(env, cfg_exp[i], soc[0]))
-        
-    # os socorristas precisam se conhecer para executar a estratégia de
-    # socorro
-    for i in range(3):
-        soc[i].set_rescuers(soc)
-        
-    env.run()
+            if id != -1:
+                victims_list.append((id, x, y, tri))
+
+    for victim in victims_list:
+        print(f"ID: {victim[0]}, Coordenadas: ({victim[1]}, {victim[2]}), Triagem: {victim[3]}")
 
 
 if __name__ == '__main__':
     print("------------------")
-    print("--- INICIO SMA ---")
+    print("--- INICIO ---")
     print("------------------")
-    # dataset com sinais vitais das vitimas
-    grid_str = "94x94"
-    vict_str = "408v"
-    vict_folder = os.path.join(".", "datasets/vict/", vict_str)
 
-    # dataset do ambiente (paredes, posicao das vitimas)
-    env_folder = path = os.path.join(".", "datasets", "env", f"{grid_str}_{vict_str}")
+    map_file = os.path.join(".", "map.csv")
+    main(map_file)
 
-    # folder das configuracoes dos agentes
-    curr = os.getcwd()
-    config_ag_folder = os.path.join(curr, "cfg")
 
-    main(vict_folder, env_folder, config_ag_folder)
